ca91a0f2942bdb6c45f8e36c9970c04133a0629a
pareshgehlot <gehlot.paresh@gmail.com>
2022-09-27 18:34:21 -0400

    dynamically filled div with buttons of choices

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-// load application
-app();
+// getting dom elements
+let choicesAvailableDiv = document.querySelector("#choicesAvailable");
+let resultDiv = document.querySelector("#result");
 
-function app(){
-    console.log("in app function");
-}
+// available choices 
+let choicesAvailable = ['rock', 'paper', 'scissor'];
+
+// fill the dom with choices
+choicesAvailable.forEach((choice)=>{
+    let button = document.createElement("button");
+    button.id = choice;
+    button.innerHTML = choice;
+    choicesAvailableDiv.appendChild(button);
+});
