f7aeb4b4fdc648586b2e763e047c0c4e1ef44484
pareshgehlot <gehlot.paresh@gmail.com>
2022-09-27 18:38:27 -0400

    added function to handle selected choice and bound to choice button click

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ let resultDiv = document.querySelector("#result");
 // available choices 
 let choicesAvailable = ['rock', 'paper', 'scissor'];
 
+let handleChoice = (e) => {
+    console.log(`you clicked : ${e.target.innerHTML}`)
+}
+
 // fill the dom with choices
 choicesAvailable.forEach((choice)=>{
     let button = document.createElement("button");
     button.id = choice;
     button.innerHTML = choice;
+    button.addEventListener('click', handleChoice);
     choicesAvailableDiv.appendChild(button);
 });
